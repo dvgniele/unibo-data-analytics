e736b8c827613b10d3b2edd585e7c555619012c5
dvgniele <ktm17@live.it>
2023-02-22 15:23:02 +0100

    add: SVM; rft: structure

---
+++ src/Fun_3/functionality_3.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+
+
+class DeepForTabularData_1:
+    def __init__(self) -> None:
+        pass
+
+
+class DeepForTabularData_2:
+    def __init__(self) -> None:
+        pass
